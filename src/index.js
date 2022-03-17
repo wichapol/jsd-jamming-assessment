c70354c7bab3f9736f412a6b2495a2ca5bdd08b7
wichapol jaimaung <wichapol.44424@gmail.com>
2022-03-17 12:15:08 +0700

    Move reset.css back to src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import '../public/reset.css';
+import './reset.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
