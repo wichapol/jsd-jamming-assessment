1711425ed23037dcc3d928c045d2d84c70cf9074
wichapol jaimaung <wichapol.44424@gmail.com>
2022-03-18 13:21:08 +0700

    Edit 5

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,7 +1,7 @@
 // TODO: Get Client ID from https://developer.spotify.com/dashboard/ and put it here
 const clientId = '2e2a79b0d4c74335a1f8539dd6c71fda';
 
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'http://music-jamming-wichapol.surge.sh';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
